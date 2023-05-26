3f7cbf61dcfe0d16c2ebbc1271870476d3592b42
Anandaprabu17 <wzly17@gmail.com>
2023-05-26 14:05:39 +0530

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+var a=1;
+var n=parseInt(prompt("Find Factorial upto"));
+for(var i=1;i<=n;i++){
+	a*=i;
+	document.write("Factorial of"+ " "+ i+ " "+ "is"+ " "+ "="+ " "+ a);
+	document.write("<br>");
+}
